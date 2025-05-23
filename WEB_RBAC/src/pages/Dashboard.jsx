9045a607fa3c373169e0231a43e9f85b9b4edf82
Admin <admin@MacBook-Air-cua-Admin.local>
2025-05-23 12:06:43 +0700

    Write custom hook: usePermissions

--- WEB_RBAC/src/pages/Dashboard.jsx
+++ WEB_RBAC/src/pages/Dashboard.jsx
@@ -15,17 +15,20 @@ import Tab from '@mui/material/Tab'
 import TabContext from '@mui/lab/TabContext'
 import TabList from '@mui/lab/TabList'
 import TabPanel from '@mui/lab/TabPanel'
+import { usePermission } from '~/hooks/usePermission'
+import { permissions } from '~/config/rbacConfig'
 
 function Dashboard() {
   const [user, setUser] = useState(null)
   const navigate = useNavigate()
   const location = useLocation()
+  const { hasPermission } = usePermission(user?.role)
 
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await authenrizedAxiosInstance.get(`${API_ROOT}/v1/dashboards/access`)
-       //console.log(res.data)
+      //console.log(res.data)
       setUser(res.data)
     }
     fetchData()
@@ -35,16 +38,16 @@ function Dashboard() {
     await handleLogoutAPI()
     // Nếu như trường hợp dùng cookie thì nhớ xoá userInfo trong Localstorage
     //localStorage.removeItem('userInfo')
-    
+
     // Điều hướng về trang login
     navigate('/login')
   }
 
   // Function đơn giản có nhiệm vụ lấy ra giá trị tab dựa theo url khi refresh trang
-  const getDefaultActiveTab = () =>{
+  const getDefaultActiveTab = () => {
     let activeTab = TAB_ULRS.DASHBOARD
     Object.values(TAB_ULRS).forEach(tab => {
-      if(location.pathname.includes(tab)) activeTab=tab
+      if (location.pathname.includes(tab)) activeTab = tab
     })
     return activeTab
   }
@@ -81,20 +84,22 @@ function Dashboard() {
       gap: 2
     }}>
       <Box as={Link} to="https://www.facebook.com/KH7NH" target="blank">
-          <Box
+        <Box
           component="img"
-          sx={{width:'100%', height:'300px', borderRadius: '6px', objectFit: 'fit-content'}}
+          sx={{ width: '100%', height: '300px', borderRadius: '6px', objectFit: 'fit-content' }}
           src={Duckhanhdev}
-          />
+        />
       </Box>
       <Alert severity="info" sx={{ '.MuiAlert-message': { overflow: 'hidden' } }}>
         Đây là trang Dashboard sau khi user:&nbsp;
         <Typography variant="span" sx={{ fontWeight: 'bold', '&:hover': { color: '#fdba26' } }}>{user?.email}</Typography>
         &nbsp; đăng nhập thành công thì mới cho truy cập vào.
       </Alert>
-      <Alert severity="success" variant='outlined' 
-      sx={{ '.MuiAlert-message': { overflow: 'hidden' }, 
-      width: {md:'max-content' } }}>
+      <Alert severity="success" variant='outlined'
+        sx={{
+          '.MuiAlert-message': { overflow: 'hidden' },
+          width: { md: 'max-content' }
+        }}>
         Role hiện tại của user là: &nbsp;
         <Typography variant="span" sx={{ fontWeight: 'bold', '&:hover': { color: '#fdba26' } }}>
           {user?.role}
@@ -105,57 +110,77 @@ function Dashboard() {
       <TabContext value={tab}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange} aria-label="Duckhanhdev">
-            <Tab label="Dashboard" value={TAB_ULRS.DASHBOARD} component={Link} to={'/dashboard'} />
-            <Tab label="Support" value={TAB_ULRS.SUPPORT} component={Link} to={'/support'} />
-            <Tab label="Messages" value={TAB_ULRS.MESSAGES} component={Link} to={'/messages'} />
-            <Tab label="Revenue" value={TAB_ULRS.REVENUE} component={Link} to={'/revenue'} />
-            <Tab label="Admin Tools" value={TAB_ULRS.ADMIN_TOOLS} component={Link} to={'/admin-tools'} />
+            {hasPermission(permissions.VIEW_DASHBOARD) &&
+              <Tab label="Dashboard" value={TAB_ULRS.DASHBOARD} component={Link} to={'/dashboard'} />
+            }
+            {hasPermission(permissions.VIEW_SUPPORT) &&
+              <Tab label="Support" value={TAB_ULRS.SUPPORT} component={Link} to={'/support'} />
+            }
+            {hasPermission(permissions.VIEW_MESSAGE) &&
+              <Tab label="Messages" value={TAB_ULRS.MESSAGES} component={Link} to={'/messages'} />
+            }
+            {hasPermission(permissions.VIEW_REVENUE) &&
+              <Tab label="Revenue" value={TAB_ULRS.REVENUE} component={Link} to={'/revenue'} />
+            }
+            {hasPermission(permissions.VIEW_ADMIN_TOOL) &&
+              <Tab label="Admin Tools" value={TAB_ULRS.ADMIN_TOOLS} component={Link} to={'/admin-tools'} />
+            }
           </TabList>
         </Box>
-        <TabPanel value={TAB_ULRS.DASHBOARD}>
-          <Alert severity='success' sx={{ width: 'max-content' }}>
-            Nội dung trang dashboard dành cho tất cả các Roles!
-          </Alert>
-        </TabPanel>
-        <TabPanel value={TAB_ULRS.SUPPORT}>
-          <Alert severity='success' sx={{ width: 'max-content' }}>
-            Nội dung trang support dành cho tất cả các Roles!
-          </Alert>
-        </TabPanel>
-        <TabPanel value={TAB_ULRS.MESSAGES}>
-          <Alert severity='info' sx={{ width: 'max-content' }}>
-            Nội dung trang messages
-          </Alert>
-        </TabPanel>
-        <TabPanel value={TAB_ULRS.REVENUE}>
-          <Alert severity='warning' sx={{ width: 'max-content' }}>
-            Nội dung trang revenue
-          </Alert>
-        </TabPanel>
-        <TabPanel value={TAB_ULRS.ADMIN_TOOLS}>
-          <Alert severity='error' sx={{ width: 'max-content' }}>
-            Nội dung trang admin
-          </Alert>
-        </TabPanel>
+        {hasPermission(permissions.VIEW_DASHBOARD) &&
+          <TabPanel value={TAB_ULRS.DASHBOARD}>
+            <Alert severity='success' sx={{ width: 'max-content' }}>
+              Nội dung trang dashboard dành cho tất cả các Roles!
+            </Alert>
+          </TabPanel>
+        }
+        {hasPermission(permissions.VIEW_SUPPORT) &&
+          <TabPanel value={TAB_ULRS.SUPPORT}>
+            <Alert severity='success' sx={{ width: 'max-content' }}>
+              Nội dung trang support dành cho tất cả các Roles!
+            </Alert>
+          </TabPanel>
+        }
+        {hasPermission(permissions.VIEW_MESSAGE) &&
+          <TabPanel value={TAB_ULRS.MESSAGES}>
+            <Alert severity='success' sx={{ width: 'max-content' }}>
+              Nội dung trang messages dành cho tất cả các Roles!
+            </Alert>
+          </TabPanel>
+        }
+        {hasPermission(permissions.VIEW_REVENUE) &&
+          <TabPanel value={TAB_ULRS.REVENUE}>
+            <Alert severity='success' sx={{ width: 'max-content' }}>
+              Nội dung trang revenue dành cho tất cả các Roles!
+            </Alert>
+          </TabPanel>
+        }
+        {hasPermission(permissions.VIEW_ADMIN_TOOL) &&
+          <TabPanel value={TAB_ULRS.ADMIN_TOOLS}>
+            <Alert severity='success' sx={{ width: 'max-content' }}>
+              Nội dung trang admin tools dành cho tất cả các Roles!
+            </Alert>
+          </TabPanel>
+        }
       </TabContext>
       <Divider />
       <Button
-      type='button'
-      variant='contained'
-      color='info'
-      size='large'
-      sx={{mt: 2, maxWidth: 'min-content', alignSelf: 'flex-end'}}
-      onClick={handleLogout}
+        type='button'
+        variant='contained'
+        color='info'
+        size='large'
+        sx={{ mt: 2, maxWidth: 'min-content', alignSelf: 'flex-end' }}
+        onClick={handleLogout}
       >
         Logout
       </Button>
-        <Box
-          component="img"
-          sx={{ width: '100%', height: '300px', borderRadius: '6px', objectFit: 'fit-content' }}
-          src={imgcontent}
-        />
+      <Box
+        component="img"
+        sx={{ width: '100%', height: '300px', borderRadius: '6px', objectFit: 'fit-content' }}
+        src={imgcontent}
+      />
     </Box>
-  
+
   )
 }
 export default Dashboard
